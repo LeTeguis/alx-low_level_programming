8849eef849cff9d54c40d42f3c99ddca4ae0c608
LeTeguis <teuguiasederis@gmail.com>
2022-07-04 16:32:29 -0700

    print_alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,7 +11,7 @@ int main(void)
 {
 	char letter;
 
-	fir (letter = 'a'; letter <= 'z'; letter++)
+	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		if (letter != 'e' && letter != 'q')
 			putchar(letter);
