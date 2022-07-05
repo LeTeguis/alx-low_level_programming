02ccd33c5ac8068f75e685285d8a9827580ce8cf
LeTeguis <teuguiasederis@gmail.com>
2022-07-05 09:28:44 +0000

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"                                                                              
+                                                                                               
+/**                                                                                            
+ * print_sign:                                                                                   
+ * @n : the negative integer                                                  
+ * Description: define if the number is negative positive or null                                       
+ * Return: 1 if @n is positive 0 is it null, -1 if is negative                                                             
+ */                                                                                            
+                                                                                               
+int print_sign(int n)                                                                           
+{                                                                                              
+        if (n < 0)
+	{
+		_putchar('-');		
+                return (-1);
+	}
+        if (n > 0)
+	{
+		_putchar('+');
+        	return (1);
+	}
+	_putchar('0');	
+        return (0);     
+}
