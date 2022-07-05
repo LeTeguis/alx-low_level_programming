cf8fd8645ca23a27e7a716d84e8d1f755b19574f
LeTeguis <teuguiasederis@gmail.com>
2022-07-04 17:46:01 -0700

    print_comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int f, s;
+
+	for (f = 0; f <= 98; f++)
+	{
+		for (s = f + 1; s <= 99; s++)
+		{
+			putchar('0' + (f/10));
+			putchar('0' + (f%10));
+			putchar(' ');
+			putchar('0' + (s/10));
+			putchar('0' + (s%10));
+
+			if(!(f == 98 && s == 99))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
