302ab173f9708bc2a71fc91c0ead859ea00a0e8d
LeTeguis <teuguiasederis@gmail.com>
2022-07-06 03:32:21 +0000

    long_fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,33 +1,32 @@
 #include <stdio.h>
 
 /**
- * main - sum of term
+ * main - long term
  *
  * Return: 0
  */
 
 int main(void)
 {
-        long int fi_2 = 0;
-        long int fi_1 = 1;
-        long int fi = 0;
+	long int fi_2 = 0;
+	long int fi_1 = 1;
+	long int fi = 0;
 
-        int i = 0;
+	int i = 0;
 
-        while (i < 98)
-        {
-                fi = fi_1 + fi_2;
-                fi_2 = fi_1;
-                fi_1 = fi;
+	while (i < 98)
+	{
+		fi = fi_1 + fi_2;
+		fi_2 = fi_1;
+		fi_1 = fi;
 
 		printf("%ld", fi);
 
 		if (i != 97)
 			printf(", ");
-                i++;
-        }
+		i++;
+	}
 
-        printf("\n");
-
-        return (0);
+	printf("\n");
+	return (0);
 }
