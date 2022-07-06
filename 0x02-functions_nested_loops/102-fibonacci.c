f8b5a0a0ca3c7ef59178b015ab5e087fdaccfeac
LeTeguis <teuguiasederis@gmail.com>
2022-07-06 03:08:28 +0000

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - fybonacy
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int fi_1 = 1;
+	int fi_2 = 0;
+	int fi;
+	int i;
+
+	for (i = 0; i < 50; i++)
+	{
+		if (i == 0)
+			printf("%d, ", fi_1);
+		else if (i == 1)
+			printf("%d, ", fi_2);
+		else
+		{
+			fi = fi_1 + fi_2;
+			printf("%d", fi);
+
+			if (i != 49)
+				printf(", ");
+
+			fi_2 = fi_1;
+			fi_1 = fi;
+		}
+	}
+
+	printf("\n");
+
+	return (0);
+}
