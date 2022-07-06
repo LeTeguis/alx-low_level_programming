8164ceab382f959099a0c0ecb2024d5416adbae7
LeTeguis <teuguiasederis@gmail.com>
2022-07-06 00:47:34 +0000

    print_sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
-/**                                                                                            
- * print_sign - signe                                                                                    
- * @n: the negative integer                                                  
- * Description: define if the number is negative positive or null                                       
- * Return: 1 if @n is positive 0 is it null, -1 if is negative                                                             
+/**
+ * print_sign - signe
+ * @n: the negative integer
+ * Description: define if the number is negative positive or null
+ * Return: 1 if @n is positive 0 is it null, -1 if is negative
  */                                                                                            
                                                                                                
 int print_sign(int n)                                                                           
