5bb67f8016b85e898735b88cd072a1e6f1c912ef
LeTeguis <teuguiasederis@gmail.com>
2022-07-08 02:09:12 +0000

    debuging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -19,5 +19,7 @@ void print_times_table(int n);
 
 int largest_number(int a, int b, int c);
 void positive_or_negative(int i);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
 
 #endif
