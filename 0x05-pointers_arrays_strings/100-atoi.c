6a4ed787460df3d0db5144c524e7fa9b574c2d77
LeTeguis <teuguiasederis@gmail.com>
2022-07-11 09:41:13 +0000

    string_to_int

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _atoi - convert string to number
+ *
+ * @s: string that content the integer
+ *
+ * Return: the integer value that detect or 0 if can  containt int
+ */
+
+int _atoi(char *s)
+{
+	int number = 0;
+	int i = 0;
+	int signe = 1;
+	int is_number = -1;
+
+	if (s == NULL)
+		return (number);
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			signe *= -1;
+		else if ('0' <= s[i] && s[i] <= '9')
+		{
+			if (is_number == 0)
+				break;
+			else if (is_number == -1)
+				is_number = 1;
+
+			number = (number * 10) + (s[i] - '0');
+		}
+		else if (is_number == 1)
+			is_number = 0;
+		i++;
+	}
+
+	return (number * signe);
+}
