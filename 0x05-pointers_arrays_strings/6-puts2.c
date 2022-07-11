0560d88ce3b3af8258ef68aa3cdf11c64a494613
LeTeguis <teuguiasederis@gmail.com>
2022-07-11 06:12:47 +0000

    print_other

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,7 +6,8 @@
  *
  * @str: string variable
  *
- * Description: prints every other character of a string, starting with the first character, followed by a new line
+ * Description: prints every other character of a string, 
+ * starting with the first character, followed by a new line
  *
  * Return: Nothing
  */
