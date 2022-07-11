bacce1c63e5e772284c0971e0058c81bffb21ebb
LeTeguis <teuguiasederis@gmail.com>
2022-07-11 05:31:51 +0000

    print_the_string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - put the string in the standart input
+ *
+ * @str: the pointer of char
+ *
+ * Description: prints a string, followed by a new line, to stdout
+ *
+ * Return: Nothing
+ */
+
+void _puts(char *str)
+{
+	if (str != NULL)
+	{
+		int p = 0;
+
+		while (str[p] != '\0')
+		{
+			_putchar(str[p]);
+			p++;
+		}
+		
+		_putchar('\n');
+	}
+}
