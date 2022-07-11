dbb191e43acf79b55b17bcb12f768313ebdcd322
LeTeguis <teuguiasederis@gmail.com>
2022-07-11 06:54:25 +0000

    print_array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -16,14 +16,14 @@ void print_array(int *a, int n)
 {
 	if (a != NULL && n > 0)
 	{
-		int i = 0;
-		
+		int i;
+
 		for (i = 0; i < n; i++)
 		{
 			printf("%d", a[i]);
 			if (i != n - 1)
 				printf(", ");
 		}
-		printf("\n");
 	}
+	printf("\n");
 }
