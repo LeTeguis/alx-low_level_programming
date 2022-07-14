0dc1364e146aed6ffd63f4e856a0a833df6883ba
LeTeguis <teuguiasederis@gmail.com>
2022-07-14 04:47:00 +0000

    update_strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,12 +15,6 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
-	if (s1 == NULL && s2 != NULL)
-		return (-1);
-	if (s1 != NULL && s2 == NULL)
-		return (1);
-	if (s1 == NULL && s2 == NULL)
-		return (0);
 
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
@@ -36,5 +30,6 @@ int _strcmp(char *s1, char *s2)
 
 	if (s1[i] == '\0')
 		return (-1);
+
 	return (1);
 }
