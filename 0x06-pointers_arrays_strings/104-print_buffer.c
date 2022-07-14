b252f04412274a1821683c26f45a5f42f921bdb2
LeTeguis <teuguiasederis@gmail.com>
2022-07-14 17:23:58 +0000

    update_printf_buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -20,34 +20,34 @@ void print_buffer(char *b, int size)
 	if (size <= 0)
 		printf("\n");
 
-	tour = (size / 10) + ((size % 10 != 0) ? 1 : 0);
+	tour = ((size) / 10) + (((size) % 10 != 0) ? 1 : 0);
 
 	for (i = 0; i < tour; i++)
 	{
 		printf("%08x: ", i*10);
-		for (j = 0; j < 20; j++)
+
+		for (j = 0; j < 10; j++)
 		{
-			int index = (i * 10) + (j % 10);
-
-			if (index >= size)
+			if (i * 10 + j < size)
 			{
-				printf("%2c", ' ');
-				if (j % 2 == 1)
-					printf(" ");
-				continue;
-			}
-
-			if (j < 10)
-			{
-				printf("%02x", b[index]);
+				printf("%02x", b[i * 10 + j]);
 				if (j % 2 == 1)
 					printf(" ");
 			}
-			
-			if (j >= 10)
+			else
 			{
-				if (b[index] >= 32)
-					printf("%c", b[index]);
+				printf("  ");
+				if (j % 2 == 1)
+					printf(" ");
+			}
+		}
+
+		for (j = 0; j < 10; j++)
+                {
+                        if (i * 10 + j < size)
+			{
+				if (b[i * 10 + j] >= 32)
+					printf("%c", b[i * 10 + j]);
 				else
 					printf(".");
 			}
