7f3fa4302dbe966ee0dd7a5b4b8541c7d801a4de
LeTeguis <teuguiasederis@gmail.com>
2022-07-14 20:11:52 +0000

    update_infinit_add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -16,65 +16,60 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i = 0, j = 0;
-	int leng1 = 0;
-	int leng2 = 0;
-	int max_leng = 0;
+	int leng_n1 = 0;
+	int leng_n2 = 0;
+	int max_leng;
+	int i = 0;
 	char retenu = '0';
 
-	while (n1[leng1] != '\0')
-		leng1++;
+	while(n1[leng_n1++] != '\0');
+	leng_n1--;
+	while(n2[leng_n2++] != '\0');
+	leng_n2--;
 
-	while (n2[leng2] != '\0')
-		leng2++;
+	max_leng = (leng_n1 < leng_n2) ? leng_n2 : leng_n1;
 
-	max_leng = leng1;
-        if (leng1 < leng2)
-                max_leng = leng2;
-
-	if (size_r - 1 < max_leng)
-	{
+	if (size_r <= max_leng)
 		return (0);
-	}
 
-	for (i = 0; i < size_r - 1; i++)
-                r[i] = '0';
+	for (i = 0; i < size_r; i++)
+	{
+		if (i <= max_leng)
+			r[i] = 0;
+		else
+			r[i] = '\0';
+	}
 
 	for (i = 0; i < max_leng; i++)
 	{
-		int tmp = retenu - '0';
+		int calcul = retenu - '0';
 
-		if (i < leng1)
-			tmp += n1[leng1 - i - 1] - '0';
-		if (i < leng2)
-			tmp += n2[leng2 - i - 1] - '0';
+		if (i < leng_n1)
+			calcul += n1[leng_n1 - i - 1] - '0';
 
-		r[size_r - i - 2] = '0' + (tmp % 10);
-		retenu = '0' + (tmp / 10);
+		if (i < leng_n2)
+			calcul += n2[leng_n2 - i - 1] - '0';
+
+		r[max_leng - i] = (calcul % 10) + '0';
+		retenu = (calcul / 10) + '0';
 	}
 
-	if (retenu > '0' && size_r < max_leng + 2)
-		return (0);
-
-	if (retenu == '0' && size_r == max_leng + 1)
+	if (retenu == '0')
+	{
+		for (i = 0; i < max_leng; i++)
+			r[i] = r[i + 1];
+		r[max_leng] = '\0';
 		return (r);
-
-	if (retenu > '0')
-	{
-		r[0] = retenu;
-		j = 1;
-
-		if (size_r == max_leng + 2)
-			return (r);
 	}
 
-	for (i = 0; i < size_r - 1; i++)
+	if (size_r <= max_leng + 1)
 	{
-		if (i >= max_leng)
-			r[j + i] = '\0';
-		else
-			r[j + i] = r[i + size_r - max_leng - j];
+		for (i = 0; i <= max_leng; i++)
+			r[i] = '\0';
+		return (0);
 	}
+
+	r[0] = retenu;
 
 	return (r);
 }
