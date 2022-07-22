0cf39fdfca68d2f4dbcf2a9b00b72a16288b06e9
LeTeguis <teuguiasederis@gmail.com>
2022-07-22 13:32:26 +0000

    update 2-args

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -7,7 +7,7 @@
  * @argv: all argument
  *
  * Description: prints all arguments it receives
- * 
+ *
  * Return: 0
  */
 
@@ -15,7 +15,7 @@ int main(int argc, char **argv)
 {
 	int i = 0;
 
-	while(i < argc)
+	while (i < argc)
 	{
 		printf("%s\n", argv[i]);
 		i++;
