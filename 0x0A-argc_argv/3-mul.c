403a64ef40f4cfda859cd95574325488c0289894
LeTeguis <teuguiasederis@gmail.com>
2022-07-22 13:40:48 +0000

    update mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - entry point
@@ -13,33 +14,8 @@
 
 int main(int argc, char **argv)
 {
-	int number_one = 0;
-	int number_two = 0;
-	int negate = 1;
-	int i = 0;
 	if (argc == 3)
-	{
-		while (argv[1][i])
-		{
-			if ('0' <= argv[1][i] && argv[1][i] <= '9')
-				number_one = (number_one) * 10 + (argv[1][i] - '0');
-			if (argv[1][i] == '-')
-				negate = -1;
-			i++;
-		}
-		number_one *= negate;
-		i = 0;
-		while (argv[2][i])
-		{
-			if ('0' <= argv[2][i] && argv[2][i] <= '9')
-				number_two = (number_two) * 10 + (argv[2][i] - '0');
-			if (argv[2][i] == '-')
-				negate = -1;
-			i++;
-		}
-		number_two *= negate;
-		printf("%d\n", (number_one * number_two));
-	}
+		printf("%d\n", (atoi(argv[1]) * atoi(argv[2])));
 	else
 		printf("Error\n");
 	return (0);
