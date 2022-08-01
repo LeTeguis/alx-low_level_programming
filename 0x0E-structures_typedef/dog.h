b17d2f2d0b5098b089df223267f5e0392588653b
LeTeguis <teuguiasederis@gmail.com>
2022-08-01 14:37:23 +0000

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef H_DOC_H
+#define H_DOC_H
+
+/**
+ * struct dog - define dog
+ * @name: dog name
+ * @age: age dog
+ * @owner: owner dog
+ *
+ * Description: define the struct to identifie dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
