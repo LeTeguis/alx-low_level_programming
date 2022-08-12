c27dc9c39550126e92b0353d537eda274dee3a55
LeTeguis <teuguiasederis@gmail.com>
2022-08-12 17:43:36 +0000

    pointer of function

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - pointer of function
+ *
+ * @name: name to print
+ * @f: function print
+ *
+ * Description: print the name
+ *
+ * Return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,6 @@
+#ifndef H_FUNC_H
+#define H_FUNC_H
+
+void print_name(char *name, void (*f)(char *));
+
+#endif
