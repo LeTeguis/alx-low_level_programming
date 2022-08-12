3840b76dab605edf375f05cb7ae822e7fea46057
LeTeguis <teuguiasederis@gmail.com>
2022-08-12 17:06:56 +0000

    struct dog advance

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,44 +16,38 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *newDog = 0;
-	int sizeName = 0;
-	int sizeOwner = 0;
+	int size_ = 0;
 
 	newDog = (dog_t *)malloc(sizeof(dog_t));
 
 	if (newDog == 0)
 		return (0);
-	while (name[sizeName])
-		sizeName++;
-	while (owner[sizeOwner])
-		sizeOwner++;
-	newDog->name = (char *)malloc(sizeof(char) * (sizeName + 1));
-	if (newDog->name)
-		while (sizeName >= 0)
-		{
-			newDog->name[sizeName - 1] = name[sizeName - 1];
-			sizeName--;
-		}
-	else
+	while (name[size_])
+		size_++;
+	while (owner[size_])
+		size_++;
+	newDog->name = (char *)malloc(sizeof(char) * (size_ + 1));
+	if (newDog->name == 0)
 	{
 		free(newDog);
 		return (0);
 	}
-	newDog->owner = (char *)malloc(sizeof(char) * (sizeOwner + 1));
-	if (newDog->owner)
-		while (sizeOwner >= 0)
-		{
-			newDog->owner[sizeOwner - 1] = owner[sizeOwner - 1];
-			sizeOwner--;
-		}
-	else
+	newDog->name[size_] = '\0';
+	while (--size_ >= 0)
+		newDog->name[size_] = name[size_];
+	size_ = 0;
+	while (owner[size_])
+		size_++;
+	newDog->owner = (char *)malloc(sizeof(char) * (size_ + 1));
+	if (newDog->owner == 0)
 	{
 		free(newDog->name);
 		free(newDog);
 		return (0);
 	}
-	newDog->name[sizeName] = '\0';
-	newDog->owner[sizeOwner] = '\0';
+	newDog->owner[size_] = '\0';
+	while (--size_ >= 0)
+		newDog->owner[size_] = owner[size_];
 	newDog->age = age;
 	return (newDog);
 }
