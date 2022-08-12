a4c0c535bde27cc6a83b81953e04a00ca2769216
LeTeguis <teuguiasederis@gmail.com>
2022-08-12 12:21:03 +0000

    struct dog advance

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+
+/**
+ * init_dog - init the dog variable
+ *
+ * @d: dog result
+ * @name: name of dog
+ * @age: the age of dog
+ * @owner: the owner of dog
+ *
+ * Description: initialize the dog data
+ *
+ * Return: nothing
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,30 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - print dog
+ *
+ * @d: dog info
+ *
+ * Description: print the dog information
+ *
+ * Return: Nothing
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == 0)
+		printf("Nothing\n");
+	else
+	{
+		if (d->name == 0)
+			printf("Name: (nil)\n");
+		else
+			printf("Name: %s\n", d->name);
+		printf("Age: %f\n", d->age);
+		if (d->owner == 0)
+			printf("Owner: (nil)\n");
+		else
+			printf("Owner: %s\n", d->owner);
+	}
+}
