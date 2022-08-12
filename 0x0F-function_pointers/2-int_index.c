b554952da7fa70f5549e1a9155e121fa20934532
LeTeguis <teuguiasederis@gmail.com>
2022-08-12 18:32:19 +0000

    pointer of function

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - find
+ *
+ * @array: array of element
+ * @size: size of element
+ * @cmp: funtion to compare
+ *
+ * Description: compare
+ *
+ * Return: index if exist -1 if not
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+		if (cmp(array[i])
+			return (i);
+	return (-1);
+}
