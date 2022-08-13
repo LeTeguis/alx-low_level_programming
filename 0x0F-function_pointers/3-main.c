7a5b47d265edcf62f38228a3fb66059937afd383
LeTeguis <teuguiasederis@gmail.com>
2022-08-13 17:06:15 +0000

    pointer de fonction

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -15,10 +15,10 @@ int main(int argc, char **argv)
 {
 	if (argc == 4)
 	{
-		int (*op)(int, int) = get_op_func(argv[2]);
+		int (*operator)(int, int) = get_op_func(argv[2]);
 
-		if (op != 0)
-			printf("%d\n", op(atoi(argv[1]), atoi(argv[3])));
+		if (operator != 0)
+			printf("%d\n", operator(atoi(argv[1]), atoi(argv[3])));
 		else
 			printf("Error\n");
 	}
