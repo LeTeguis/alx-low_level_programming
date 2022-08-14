184da15b41231f10dafabec32a02fbbbb6c3943d
LeTeguis <teuguiasederis@gmail.com>
2022-08-14 13:27:38 +0000

    variadic

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -27,7 +27,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (chaine == 0)
 			printf("(nil)");
 		else
-			printf("%s", va_arg(ap, char *));
+			printf("%s", chaine);
 		if (separator != 0 && i != n - 1)
 			printf("%s", separator);
 	}
