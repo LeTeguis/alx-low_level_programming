c89c890f7d53ba0b7e674a487889153586b1462d
LeTeguis <teuguiasederis@gmail.com>
2022-08-18 06:42:42 +0000

    binary_to_uint

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_binary - print
+ *
+ * @n: decimal number
+ *
+ * Description: prints the binary representation of a number
+ *
+ * Return: nothing
+ */
+
+void print_binary(unsigned long int n)
+{
+	if (n == 0)
+		_putchar('0');
+	else if (n == 1)
+		_putchar('1');
+	else
+	{
+		print_binary(n >> 1);
+		_putchar((n & 1) + '0');
+	}
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -87,5 +87,6 @@ int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
 
 #endif
