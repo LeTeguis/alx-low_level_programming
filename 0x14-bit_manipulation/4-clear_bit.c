c22bb3d78ca019484dcc9ca89da6709e97096b6b
LeTeguis <teuguiasederis@gmail.com>
2022-08-18 07:30:25 +0000

    clear_bit

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * clear_bit - set
+ *
+ * @n: decimal number
+ * @index: index to set
+ *
+ * Description: set the index value at 0
+ *
+ * Return: 1 if it working or -1 or not
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (n == 0)
+		return (-1);
+	if (*n == 0)
+		return (1);
+	if (((*n >> index) & 1) == 1)
+		*n = *n & (~(1 << index));
+	return (1);
+}
