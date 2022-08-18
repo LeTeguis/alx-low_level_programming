b96088a1859beec07184e32cab981c3e8608f15e
LeTeguis <teuguiasederis@gmail.com>
2022-08-18 15:16:55 +0000

    get_bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -13,13 +13,7 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (n == 0 || n == 1)
-	{
-		if (index == 0)
-			return (n);
+	if (index >= (sizeof(unsigned int)))
 		return (-1);
-	}
-	if (index == 0)
-		return (n & 1);
-	return (get_bit((n >> 1), (index - 1)));
+	return ((n & (1 << index)) ? 1 : 0);
 }
