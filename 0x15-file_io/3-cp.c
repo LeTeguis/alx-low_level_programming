dcb8dd14ebda7071ee25f7b4c22b539a4a870e8a
LeTeguis <teuguiasederis@gmail.com>
2022-08-22 18:23:43 +0000

    copy_from_to

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -76,7 +76,7 @@ int main(int argc, char **argv)
 	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC,
 			S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	exit_error(file_to, 98, argv[2], file_from, -1);
-	while (len_read > 0)
+	while (len_read == 1024)
 	{
 		len_read = read(file_from, content_read, len_read);
 		exit_error(len_read, 98, argv[1], file_from, file_to);
