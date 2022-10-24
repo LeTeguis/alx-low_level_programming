55521db03a502041a07237336067b5eecb1aa7be
LeTeguis <teuguiasederis@gmail.com>
2022-10-24 13:58:14 +0000

    0. >>> ht = {}

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_hash = 0;
+
+	new_hash = (hash_table_t *)malloc(sizeof(hash_table_t));
+	if (new_hash == 0)
+		return (0);
+	new_hash->array = (hash_node_t **)malloc(sizeof(hash_node_t *) * size);
+	if (new_hash->array == 0)
+	{
+		free(new_hash);
+		return (0);
+	}
+	new_hash->size = size;
+	return (new_hash);
+}
