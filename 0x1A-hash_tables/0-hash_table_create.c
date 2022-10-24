87725b6fe9c2c71fbbe8b64ac9209220a7e91e85
LeTeguis <teuguiasederis@gmail.com>
2022-10-24 14:00:23 +0000

    0. >>> ht = {}

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,6 +1,15 @@
 #include "hash_tables.h"
 #include <stdlib.h>
 
+/**
+ * hash_table_create - new hash
+ *
+ * @size: size of hash
+ *
+ * Description: create new instance of hash table
+ *
+ * Return: new hash table or NULL
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_hash = 0;
